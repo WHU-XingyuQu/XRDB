a6da011b56a6ae4fb4a0d6b0bef32ba96fcbea6d
Cookie <1744870019@qq.com>
2026-02-17 14:32:01 +0800

    complete Parser

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -1,2 +1,4 @@
 pub mod parser;
 pub mod types;
+pub mod plan;
+mod schema;
--- src/sql/types/mod.rs
+++ src/sql/types/mod.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use crate::sql::parser::ast::{Consts,Expression};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum DataType {
@@ -6,4 +7,35 @@ pub enum DataType {
     Integer,
     Float,
     String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum Value {
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+    Null,
+}
+
+impl Value {
+    pub fn from_expression(expr:Expression) -> Self {
+        match expr{
+            Expression::Consts(Consts::Boolean(b)) => Self::Boolean(b),
+            Expression::Consts(Consts::Integer(i)) => Self::Integer(i),
+            Expression::Consts(Consts::Float(f)) => Self::Float(f),
+            Expression::Consts(Consts::String(s)) => Self::String(s),
+            Expression::Consts(Consts::Null) => Self::Null,
+        }
+    }
+
+    pub fn datatype(&self) -> Option<DataType> {
+        match self{
+            Self::Boolean(_) => Some(DataType::Boolean),
+            Self::Integer(_) => Some(DataType::Integer),
+            Self::Float(_) => Some(DataType::Float),
+            Self::String(_) => Some(DataType::String),
+            _ => None,
+        }
+    }
 }
